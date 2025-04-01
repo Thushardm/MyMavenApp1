a5991df9615392db83c73469b761644bc9462486
Thushardm <tushardm123@gmail.com>
2025-04-01 06:28:40 +0000

    initial commit

---
+++ src/main/java/com/example/App3.java
@@ -0,0 +1,11 @@
+package com.example;
+import java.util.Scanner;
+
+public class App3{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
